1aaf6dfe90a428a80c8e0396512221fcc01d73f3
CEREN SUSUZ <ceren199704@hotmail.com>
2021-05-25 20:57:24 +0300

    mernis refactoring

--- hrms/src/main/java/com/hrms/business/concretes/DepartmentManager.java
+++ hrms/src/main/java/com/hrms/business/concretes/DepartmentManager.java
@@ -1,6 +1,5 @@
 package com.hrms.business.concretes;
 
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- hrms/src/main/java/com/hrms/core/adapters/mernis/MernisServiceAdapter.java
+++ hrms/src/main/java/com/hrms/core/adapters/mernis/MernisServiceAdapter.java
@@ -2,28 +2,27 @@ package com.hrms.core.adapters.mernis;
 
 import org.springframework.stereotype.Service;
 
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
+
 @Service
 public class MernisServiceAdapter implements UserCheckService {
 
 	@Override
-	public boolean validate(String firstName, String lastName,long nationalityId, int birthYear){
-	return true;
+	public boolean validate(String firstName, String lastName, long nationalityId, int birthYear) {
+		
+		KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();
+		boolean result = false;
+			 try {
+		            return kpsPublicSoapProxy.TCKimlikNoDogrula(
+		                    nationalityId,
+		                    firstName,
+		                    lastName,
+		                    birthYear);
+		        } catch (Exception ex) {
+		            ex.printStackTrace();
+		        }
+		        
+	           return result;
 	}
-
-//	KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();
-//	
-//	public boolean validate(String firstName, String lastName,long nationalityId, int birthYear) {
-//		 try {
-//	            return kpsPublicSoapProxy.TCKimlikNoDogrula(
-//	                    nationalityId,
-//	                    firstName,
-//	                    lastName),
-//	                    birthYear);
-//	        } catch (Exception e) {
-//	            ex.printStackTrace();
-//	        }
-//	        
-//           return result;
-//	}
 
 }
